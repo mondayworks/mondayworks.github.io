846d31fd043235a1ef292d98db231b3c94967bce
Jens Andresen <jensandresen@gmail.com>
2018-08-31 14:21:33 +0200

    wip: almost there

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import "bulma";
-import "./main.scss";
+import "./styles/main.scss";
 import ScrollReveal from "scrollreveal";
 
-// window.sr = ScrollReveal();
-// sr.reveal(".fadein");
-// sr.reveal(".fadein-left", { origin: "left", distance: "100%", duration: "1000", delay: "500" });
-// sr.reveal(".fadein-right", { origin: "right", distance: "100%", duration: "1000", delay: "500" });
+window.sr = ScrollReveal();
+sr.reveal(".fadein", { delay: 200 });
+// sr.reveal(".fadein-delayed", { delay: "200" }, 150);
+sr.reveal(".fadein-delayed", { delay: "200" }, 500);
+sr.reveal(".fadein-left", { origin: "left", distance: "100%", duration: "1000", delay: "500" });
+sr.reveal(".fadein-right", { origin: "right", distance: "100%", duration: "1000", delay: "500" });
