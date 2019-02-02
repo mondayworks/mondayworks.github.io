6084008d6cc7f7efe25ac28a0c438b7d7d4391be
Jens Andresen <jensandresen@gmail.com>
2019-02-02 08:46:55 +0100

    change layout of services section

--- src/main.js
+++ src/main.js
@@ -7,4 +7,5 @@ sr.reveal(".fadein", { delay: 200 });
 // sr.reveal(".fadein-delayed", { delay: "200" }, 150);
 sr.reveal(".fadein-delayed", { delay: "200" }, 500);
 sr.reveal(".fadein-left", { origin: "left", distance: "100%", duration: "1000", delay: "500" });
+sr.reveal(".fadein-left-sync", { origin: "left", delay: "50" }, 250);
 sr.reveal(".fadein-right", { origin: "right", distance: "100%", duration: "1000", delay: "500" });
